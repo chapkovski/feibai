7d810d6c08920c543fa9e56bc3d009fed9673502
chapkovski <chapkovski@gmail.com>
2017-07-09 14:55:29 +0200

    first commit

---
+++ feibai/models.py
@@ -0,0 +1,32 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'feibai'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    # the field to store the array of shown numbers:
+    rnd_num_shown = models.CharField()
+    # the field to store the number of clicks:
+    rnd_num_clicked = models.IntegerField()
---
+++ feibai/views.py
@@ -0,0 +1,26 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class MyPage(Page):
+    form_model = models.Player
+    form_fields = ['rnd_num_shown', 'rnd_num_clicked']
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [
+    MyPage,
+    ResultsWaitPage,
+    Results
+]
